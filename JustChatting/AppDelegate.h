0dc9449182ff840e7094ecaab90c64ab807ebcab
Lemonade <842476697@qq.com>
2017-08-31 09:17:02 +0800

    Initial Commit

---
+++ JustChatting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JustChatting
+//
+//  Created by Lemonade on 2017/8/31.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
