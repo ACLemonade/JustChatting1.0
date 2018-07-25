1c3084c4aa67632e1389c1c8ebfa1832b2a9a667
Lemonade <842476697@qq.com>
2018-07-25 09:36:45 +0800

    解决Xcode9更新后出现的一些bug

---
+++ JustChatting/Main/Message/Controller/MessageLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessageLocationViewController.h
+//  JustChatting
+//
+//  Created by Lemonade on 2017/12/5.
+//  Copyright © 2017年 Lemonade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageLocationViewController : UIViewController
+
+@end
